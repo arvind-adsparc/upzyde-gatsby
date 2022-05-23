95e39e7ff66df2f85fb4cf538a55468159b3dcc4
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-23 17:55:31 +0530

    addition of footer content

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer goes here</footer>;
+  return (
+    <footer>
+      <div className="logo">
+        <img
+          src="https://www.upzyde.com/wp-content/uploads/2019/11/logo.png"
+          alt=""
+        />
+      </div>
+
+      <div className="social-links"></div>
+
+      <div className="footer-txt">
+        <div className="privacy-policy">Privacy Policy</div>
+        <div className="copyright">2022 © Copyright. All rights reserved.</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
