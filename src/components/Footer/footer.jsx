a4b0fbd213f787a200e8055e3cb0c673854ee298
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-23 18:26:17 +0530

    footer social links mapping

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,20 +1,47 @@
 import React from "react";
+import { TwitterOutlined, FacebookFilled } from "@ant-design/icons";
+
+import "./footer.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="logo">
-        <img
-          src="https://www.upzyde.com/wp-content/uploads/2019/11/logo.png"
-          alt=""
-        />
-      </div>
+      <div className="foot-container">
+        <div className="foot-logo">
+          <img
+            src="https://www.upzyde.com/wp-content/uploads/2019/11/logo.png"
+            alt=""
+          />
+        </div>
 
-      <div className="social-links"></div>
+        <div className="social-links">
+          <div>
+            <a
+              href="https://twitter.com/upzyde"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <TwitterOutlined />
+            </a>
+          </div>
 
-      <div className="footer-txt">
-        <div className="privacy-policy">Privacy Policy</div>
-        <div className="copyright">2022 © Copyright. All rights reserved.</div>
+          <div>
+            <a
+              href="https://www.facebook.com/Upzyde/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FacebookFilled />
+            </a>
+          </div>
+        </div>
+
+        <div className="footer-txt">
+          <div className="privacy-policy">Privacy Policy</div>
+          <div className="copyright">
+            2022 © Copyright. All rights reserved.
+          </div>
+        </div>
       </div>
     </footer>
   );
