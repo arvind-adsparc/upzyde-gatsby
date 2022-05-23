928ae688e781cc9d94d4f3b426cbfc9519ecb771
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-23 15:11:44 +0530

    addition of header component

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../Header/header";
 import "./layout.scss";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <div>Header Goes here</div>
+      <Header />
       <div className="page-content container">{children}</div>
       <div>Footer goes here</div>
     </div>
