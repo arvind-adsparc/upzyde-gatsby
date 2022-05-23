9bce76520d604a7ff11d5c06c43a595d480c6aee
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-23 15:47:17 +0530

    addition of global css in gatsby browser file

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import "./header.scss";
 
 const Header = () => {
   return (
     <header>
-      <div className="logo"></div>
+      <div className="container flex-container">
+        <div className="logo">
+          <img
+            src="https://www.upzyde.com/wp-content/uploads/2019/11/logo.png"
+            alt=""
+          />
+        </div>
 
-      <nav className="nav-links">
-        <div className="link">HOME</div>
-        <div className="link">CHANNELS</div>
-        <div className="link">BENEFITS</div>
-        <div className="link">HOME</div>
-      </nav>
+        <nav className="nav-links">
+          <div className="link">HOME</div>
+          <div className="link">CHANNELS</div>
+          <div className="link">BENEFITS</div>
+          <div className="link">HOME</div>
+        </nav>
+      </div>
     </header>
   );
 };
