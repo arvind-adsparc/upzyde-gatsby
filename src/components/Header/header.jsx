ddcd268afd8cc4bee59b52b2763988595a851dc9
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-23 15:22:45 +0530

    addition of global variables

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-  return <header>Header goes here..</header>;
+  return (
+    <header>
+      <div className="logo"></div>
+
+      <nav className="nav-links">
+        <div className="link">HOME</div>
+        <div className="link">CHANNELS</div>
+        <div className="link">BENEFITS</div>
+        <div className="link">HOME</div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
