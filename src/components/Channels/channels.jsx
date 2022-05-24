f81100de775c6792544010ae2dc04895d892fe16
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-24 16:38:34 +0530

    addition of card component in channels comp

--- src/components/Channels/channels.jsx
+++ src/components/Channels/channels.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { data } from "./data";
+
 import "./channels.scss";
 
 const Channels = () => {
@@ -6,6 +9,28 @@ const Channels = () => {
     <section className="channels-section">
       <div className="inner-container">
         <h2>Our Header Bidding Channels</h2>
+
+        <div className="channels-content">
+          {data.map((info) => {
+            return (
+              <div className="card-container" key={info.title}>
+                <div className="card-img">
+                  <StaticImage
+                    className=""
+                    src="https://res.cloudinary.com/dqove2qhg/image/upload/v1653389420/Upzyde/display_k1kevw.webp"
+                    alt={info.title}
+                    title={info.title}
+                    placeholder="none"
+                  />
+                </div>
+
+                <div className="card-title">{info.title}</div>
+
+                <div className="card-description">{info.description}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Channels/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389420/Upzyde/display_k1kevw.webp",
+    title: "Display",
+    description:
+      "The backbone of header bidding. We implement and managing wrapper solutions and monetise the full display inventory of publishers.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389420/Upzyde/display_k1kevw.webp",
+    title: "Display",
+    description:
+      "The backbone of header bidding. We implement and managing wrapper solutions and monetise the full display inventory of publishers.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389420/Upzyde/display_k1kevw.webp",
+    title: "Display",
+    description:
+      "The backbone of header bidding. We implement and managing wrapper solutions and monetise the full display inventory of publishers.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389420/Upzyde/display_k1kevw.webp",
+    title: "Display",
+    description:
+      "The backbone of header bidding. We implement and managing wrapper solutions and monetise the full display inventory of publishers.",
+  },
+];
