b54bccc52494160a99cbae74250453e024513b9b
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-26 11:10:21 +0530

    addition of benefits json data

---
+++ src/components/Benefits/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653391895/Upzyde/display_kgco0e.webp",
+    title: "Guaranteed revenue increase",
+    description:
+      "Through the Upzyde header bidding set up revenue will increase by up to 50%, depending on your current set up. Header bidding itself, plus the additional revenue boosting functionalities of the Upzyde platform will ensure a significant boost in revenue.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653391896/Upzyde/video_uf03nf.webp",
+    title: "No long term commitments",
+    description:
+      "Change is the only constant and we understand publishers want to be flexible. At the same time we know our solutions work and know publishers do continue to use our solutions for extended periods.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389423/Upzyde/mobile-inapp_ejqhfp.webp",
+    title: "On time payments",
+    description:
+      "It talks longer and longer for publishers to get paid. Not for Upzyde publishers! Upzyde payments are like clockwork and publishers can trust funds to be paid on time, always.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389424/Upzyde/mobile-amp_rtkmxh.webp",
+    title: "In-dept expertise",
+    description:
+      "Publisher success is guaranteed by the knowledge and experience of the Upzyde team and the capability to find continues optimisation opportunities.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389424/Upzyde/mobile-amp_rtkmxh.webp",
+    title: "Ensured Ad Quality",
+    description:
+      "Publishers will only receive demand from tier one SSPs and agencies.",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/dqove2qhg/image/upload/v1653389424/Upzyde/mobile-amp_rtkmxh.webp",
+    title: "Open Platform Architecture",
+    description:
+      "The Upzyde solution is built on the prebid standard and seamlessly connects to Google Ad Manager, Amazon A9 and all IAB compliant CPMs.",
+  },
+];
