2850b018586f01c310a1e6a2e0338977d061fc64
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-26 18:51:44 +0530

    addition of contact address

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,12 +4,19 @@ import "./contact.scss";
 const Contact = () => {
   return (
     <section className="contact-section">
-      <h2>CONTACT US</h2>
+      <div className="inner-container">
+        <h2>CONTACT US</h2>
 
-      <div className="contact-content">
-        <div className="address"></div>
+        <div className="contact-content">
+          <div className="detail">
+            <div className="company-name">Upzyde B.V.</div>
+            <div className="address">
+              Fonteinlaan 5a, 2012 JT Haarlem, The Netherlands
+            </div>
+          </div>
 
-        <div className="form"></div>
+          <div className="form"></div>
+        </div>
       </div>
     </section>
   );
