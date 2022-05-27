67490cdb7e11eb02afd3aae85c28a730b0879e43
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-27 15:17:01 +0530

    addition of privacy policy styles

--- src/pages/privacy-policy/index.js
+++ src/pages/privacy-policy/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../../components/Layout/layout";
 import Seo from "../../components/SEO/seo";
+import "./styles.scss";
 
 const PrivacyPolicy = () => {
   return (
@@ -11,7 +12,9 @@ const PrivacyPolicy = () => {
         description="The world around us is constantly changing and the speed at which it changes increases rapidly. In this dynamic and challenging environment, Upzyde connects the dots to ensure publishers maximise yield of their inventory."
       />
       <Layout>
-        <div className="inner-container">conent goes here</div>
+        <div className="inner-container privacy-container">
+          <h2>Privacy Policy</h2>
+        </div>
       </Layout>
     </React.Fragment>
   );
