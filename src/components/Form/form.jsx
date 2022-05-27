310a2b08a89502b339c175167f3cb247f0daed6c
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-27 13:16:45 +0530

    addition of default font size

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Input from "../Input/input";
 
 const Form = () => {
-  return <div className="contact-form">Form goes here!!</div>;
+  return (
+    <div className="contact-form">
+      <Input name="name" label="Name" type="text" isRequired={true} />
+      <Input name="email" label="Email" type="email" isRequired={true} />
+      <Input name="subject" label="Subject" type="text" isRequired={false} />
+    </div>
+  );
 };
 
 export default Form;
