8e622638032e394304769e99094f1bd151fa2aeb
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-05-30 14:44:03 +0530

    hero variant change

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-// import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 import "./hero.scss";
 
 const Hero = () => {
   return (
     <section className="hero-section">
+      <div className="hero-img">
+        <StaticImage
+          src="https://res.cloudinary.com/dqove2qhg/image/upload/v1653636304/Upzyde/section01-bg_tqficl.webp"
+          alt=""
+        />
+      </div>
       <div className="hero-content  inner-container">
         <h1>CHANGE IS THE ONLY CONSTANT</h1>
 
